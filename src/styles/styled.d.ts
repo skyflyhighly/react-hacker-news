a19cd0a4b5c5d44bcdff074c78de95866bc0fa87
skyflyhighly <skyflyhighly@gmail.com>
2023-11-13 22:35:09 +0800

    feat/styles: Add styled breakpoints, theme

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,40 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: "#FAFAFA";
+      secondary: "#FFC80A";
+      tertiary: "#303030";
+      background: "#ffffff";
+      text: "#000000";
+      primaryTextColor: "#594F43";
+      secondaryTextColor: "#777777";
+      inputPlaceholder: "#C7C7C7";
+      darkGrayText: "#303030";
+      darkText: "#1A1A1A";
+      border: "#ddd";
+      black: "#000000";
+      white: "#ffffff";
+      blue: "#007BFF";
+      dark: "";
+      medium: "";
+      light: "";
+      danger: "";
+      success: "#66A15A";
+    };
+    fonts: {
+      anekMalayalam: "Anek Malayalam";
+    };
+    paddings: {
+      container: "15px";
+      pageTop: "30px";
+    };
+    margins: {
+      pageTop: "30px";
+      cardBottom: "5px";
+    };
+  }
+}
