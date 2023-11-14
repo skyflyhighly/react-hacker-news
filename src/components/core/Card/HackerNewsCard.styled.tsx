bed30adf869b9e712785f1b34f67a21009e37298
skyflyhighly <skyflyhighly@gmail.com>
2023-11-14 11:43:44 +0800

    refactor/HackerNewsCard: Update the UI - Make responsive - Add animation - Add same image

--- src/components/core/Card/HackerNewsCard.styled.tsx
+++ src/components/core/Card/HackerNewsCard.styled.tsx
@@ -1,32 +1,90 @@
 import styled from "styled-components";
 
-import { devices } from "../../../styles/BreakPoints";
-import { theme } from "../../../styles/Theme";
+import { device } from "../../../styles/BreakPoints";
 
 export const StoryContainer = styled.div`
-  padding-bottom: ${({ theme }) => theme.paddings.container};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+  width: 100%;
+  max-width: 700px;
+  margin-bottom: ${({ theme }) => theme.margins.cardBottom};
+  display: flex;
+  color: ${({ theme }) => theme.colors.primary};
+
+  @media ${device.sm} {
+    flex-direction: column;
+    box-shadow: 0 8px 0 0 ${({ theme }) => theme.colors.border};
+    border-radius: 8px;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  flex: 4;
+  overflow: hidden;
+`;
+
+export const Image = styled.img`
+  border-radius: 5px;
+  aspect-ratio: auto 728 / 380;
+  transition: scale 0.4s;
+  transition-timing-function: ease-in-out;
+
+  &:hover {
+    scale: 1.05;
+  }
+`;
+
+export const TextContainer = styled.div`
+  flex: 6;
+  padding-left: ${({ theme }) => theme.paddings.container};
+
+  @media ${device.sm} {
+    padding: 5px 0 10px 5px;
+  }
 `;
 
 export const Title = styled.h2`
-  font-size: 18px;
-  margin-bottom: ${({ theme }) => theme.margins.cardBottom};
+  font-size: 19px;
+  font-weight: 900;
+  line-height: 31px;
+  margin-bottom: 10px;
+  letter-spacing: -0.15px;
 `;
 
-export const Url = styled.p`
-  color: ${({ theme }) => theme.colors.blue};
+export const UrlContainer = styled.div`
+  padding: 8px 0;
+  margin-bottom: 10px;
+`;
+
+export const Url = styled.a`
+  color: ${({ theme }) => theme.colors.primary};
+  text-decoration: none;
+  word-break: break-all;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const MetaContainer = styled.div`
+  display: flex;
+  padding: 8px 0;
 `;
 
 export const TimeStamp = styled.span`
+  display: flex;
+  gap: 5px;
   font-size: 14px;
-  color: ${({ theme }) => theme.colors.secondaryTextColor};
-`;
-
-export const Author = styled.span`
-  margin-top: 5px;
 `;
 
 export const Score = styled.span`
   font-size: 14px;
-  color: ${({ theme }) => theme.colors.secondaryTextColor};
+  margin-left: 25px;
+`;
+
+export const Author = styled.span`
+  font-size: 14px;
+`;
+
+export const KarmaScore = styled.span`
+  font-size: 14px;
+  margin-left: 25px;
 `;
--- src/components/core/Card/HackerNewsCard.tsx
+++ src/components/core/Card/HackerNewsCard.tsx
@@ -1,31 +1,54 @@
 import React from "react";
 
-import {
-  StoryContainer,
-  Title,
-  Url,
-  TimeStamp,
-  Author,
-  Score,
-} from "./HackerNewsCard.styled";
 import { formatTimestamp } from "../../../utils";
 import { HackerNews } from "../../../types";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
+
+import {
+  StoryContainer,
+  ImageContainer,
+  Image,
+  TextContainer,
+  Title,
+  UrlContainer,
+  Url,
+  MetaContainer,
+  Score,
+  TimeStamp,
+  Author,
+  KarmaScore,
+} from "./HackerNewsCard.styled";
 
 const HackerNewsCard: React.FC<HackerNews> = (props) => {
   return (
     <>
       <StoryContainer key={props.id}>
-        <Title>{props.title}</Title>
-        <Url>
-          <a href={props.url} target="_blank" rel="noopener noreferrer">
-            {props.url}
-          </a>
-        </Url>
-        <TimeStamp>{formatTimestamp(props.timestamp)}</TimeStamp>
-        <Score>Score: {props.score}</Score>
-        <Author>
-          By {props.authorId} : {props.authorKarmaScore}
-        </Author>
+        <ImageContainer>
+          <Image src={props.image} />
+        </ImageContainer>
+        <TextContainer>
+          <Title>{props.title}</Title>
+          {props.url !== undefined && (
+            <UrlContainer>
+              <Url href={props.url} target="_blank" rel="noopener noreferrer">
+                {props.url}
+              </Url>
+            </UrlContainer>
+          )}
+          <MetaContainer>
+            <TimeStamp>
+              <FontAwesomeIcon icon={faCalendar} size="1x" />
+              {formatTimestamp(props.timestamp)}
+            </TimeStamp>
+            <Score>Story Score: {props.score}</Score>
+          </MetaContainer>
+          <MetaContainer>
+            <Author>Author: {props.authorId}</Author>
+            <KarmaScore>Author Score: {props.authorKarmaScore}</KarmaScore>
+          </MetaContainer>
+        </TextContainer>
       </StoryContainer>
     </>
   );
